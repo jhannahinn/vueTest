620dea8bda1939cf7fec885338f922bfc9e63377
root <root@DESKTOP-47LGCUO.localdomain>
2022-04-04 18:27:35 +0900

    컴포넌트 사용 2022-04-04

---
+++ src/components/asset/MyLabel.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+    <div class="label" @click="open = !open">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            open: false
+        }
+    }
+    // methods: {
+        
+    // }
+}
+</script>
+
+<style lang="css" scoped>
+.label {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 10px;
+
+    position: absolute;
+    width: 48px;
+    height: 10px;
+    left: 20px;
+    top: 20px;
+
+
+    background: #3192CA;
+    border-radius: 4px;
+}
+</style>
--- src/views/intro/Splash.vue
+++ src/views/intro/Splash.vue
@@ -1,27 +1,18 @@
 <template lang="html">
-    <button class="label" @click="open = !open">
-    </button>
+    <my-label></my-label>
 </template>
 
 <script>
+import MyLabel from '../../components/asset/MyLabel.vue'
+
 export default {
-    data() {
-        return {
-            open: false
-        }
+    components: {
+        MyLabel
     }
-    // methods: {
-        
-    // }
+   
 }
 </script>
 
 <style lang="css" scoped>
-.label {
-    background-color: #3192CA;
-    width: 48px;
-    height: 10px;
-    border: none;
-    border-radius: 4px;
-}
+
 </style>
